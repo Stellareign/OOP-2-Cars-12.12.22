55ff2a6408acb38da3b390cc72b7c2c7c802ebc4
Stellareign <tuguran@mail.ru>
2022-12-18 17:23:42 +0300

    Вроде готово.

--- src/Car.java
+++ src/Car.java
@@ -1,43 +1,222 @@
+import java.time.LocalDate;
+
 public class Car {
-    String brand;
-    String model;
+    final String brand;
+    final String model;
     double engineVolume;
     String color;
-    int year;
-    String country;
-// невозможно нормально работать
-    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        if(brand == null || brand.isEmpty()|| brand.isBlank()){
-            this.brand =" default, ";
+    final int releasYear;
+    final String country;
+    // новые данные
+    String transmission;
+    final String bodyType;
+    final int numberOfSeats;
+    String regNumber;
+    String tire;
+    private Key key;
+
+    //    Key key = new Key("", "");
+    public Car(String brand, String model, double engineVolume, String color, int year, String country, String transmission, String bodyType,
+               int numberOfSeats, String regNumber, String tire, Key key) {
+        if (brand != null && !brand.isEmpty() && !brand.isBlank()) {
+            this.brand = brand;
+        } else {
+            this.brand = " default, ";
         }
-        else{
-        this.brand = brand;}
-        if (model == null || model.isBlank() || model.isEmpty()){
+        if (model == null || model.isBlank() || model.isEmpty()) {
             this.model = " default ";
-        } else{
-        this.model = model;}
-        if(engineVolume >= 0){
-        this.engineVolume = engineVolume;}
-        else {this.engineVolume = 1.5;}
-        if(color == null || color.isEmpty() || color.isBlank()){
+        } else {
+            this.model = model;
+        }
+        if (engineVolume >= 0) {
+            this.engineVolume = engineVolume;
+        } else {
+            this.engineVolume = 1.5;
+        }
+        if (color == null || color.isEmpty() || color.isBlank()) {
             this.color = " белый ";
+        } else {
+            this.color = color;
         }
-        else{
-        this.color = color;}
-        if(year <= 0){
-            this.year = 2000;
+        if (year <= 0) {
+            this.releasYear = 2000;
+        } else {
+            this.releasYear = year;
         }
-        else{
-        this.year = year;}
-        if(country == null || country.isBlank() || country.isEmpty()){
-            this.country = " default ";}
-        else{
-        this.country = country;}
+        if (country == null || country.isBlank() || country.isEmpty()) {
+            this.country = " default ";
+        } else {
+            this.country = country;
+        }
+
+        if (transmission != null && !transmission.isEmpty() && !transmission.isBlank()) {
+            this.transmission = transmission;
+        } else {
+            this.transmission = "МКПП";
+        }
+        if (bodyType != null && !bodyType.isBlank() && !bodyType.isEmpty()) {
+            this.bodyType = bodyType;
+        } else {
+            this.bodyType = "седан";
+        }
+        if (numberOfSeats != 0) {
+            this.numberOfSeats = numberOfSeats;
+        } else {
+            this.numberOfSeats = 4;
+        }
+        this.tire = changeTires();
+        this.regNumber = regNumber;
+        this.key = key;
     }
 
+
+    // метод смены резины
+    private String changeTires() {
+        int month = LocalDate.now().getMonthValue();
+        if (month <= 4 || month > 9) {
+            return "зима";
+        } else {
+            return "лето";
+        }
+    }
+
+    // проверка номера
+    //X000XX000
+    private String checkRegNumber() {
+        if (regNumber != null && !regNumber.isEmpty() && !regNumber.isBlank() && regNumber.length() != 9) {
+            return " номер указан неверно ";
+        }
+        if (regNumber == null || regNumber.isBlank() || regNumber.isEmpty()) {
+            return " X000XX_000";
+
+        }
+        char[] chars = regNumber.toCharArray();
+        if (!Character.isAlphabetic(chars[0]) || !Character.isAlphabetic(chars[4]) || !Character.isAlphabetic(chars[5])) {
+            return " номер указан неверно ";
+        }
+        if (!Character.isDigit(chars[1]) || !Character.isDigit(chars[2]) || !Character.isDigit(chars[3]) || !Character.isDigit(chars[6])
+                || !Character.isDigit(chars[7]) || !Character.isDigit(chars[8])) {
+            return " номер указан неверно ";
+        }
+        return regNumber;
+
+    }
+
+
+    public static class Key {
+
+        private final String remoteEngineStart;
+        private final String keylessAccess;
+
+        public Key(String remoteEngineStart, String keylessAccess) {
+            if (remoteEngineStart != null && !remoteEngineStart.isBlank() && !remoteEngineStart.isEmpty()) {
+                this.remoteEngineStart = remoteEngineStart;
+            } else {
+                this.remoteEngineStart = "нет";
+            }
+            if (keylessAccess != null && !keylessAccess.isEmpty() && !keylessAccess.isBlank()) {
+                this.keylessAccess = keylessAccess;
+            } else {
+                this.keylessAccess = "нет";
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "удалённый запуск двигателя: " + remoteEngineStart + ", доступ без ключа: " + keylessAccess;
+        }
+    }
+
+    public Car setKey(Key key) {
+        this.key = key;
+        return this;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    //      String transmission;
+    //    final String bodyType = null;
+    //    final int numberOfSeats = 0;
+    //    String regNumber;
+    //    String tire;
     @Override
     public String toString() {
-        return "Автомобтль " + brand + " " + model + ", объём движка " + engineVolume + " л, цвет " +
-                 color + ", год выпуска " + year +" г., cтрана производства " + country +".";
+        return "Автомобиль " + brand + " " + model + "; объём движка " + engineVolume + " л; цвет " +
+                color + "; год выпуска: " + releasYear + " г.; cтрана производства: " + country + "; КПП: " + transmission + "; тип кузова: " + bodyType +
+                "; количество мест: " + numberOfSeats + "; гос.номер: " + checkRegNumber() + "; тип покрышек: " + tire + "; удалённый доступ: " + getKey();
+    }
+
+
+    // ГЕТТЕРЫ/СЕТТЕРЫ:
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public Car setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+        return this;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Car setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public int getReleasYear() {
+        return releasYear;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public Car setTransmission(String transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public String getRegNumber() {
+
+        return regNumber;
+    }
+
+    public Car setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+        return this;
+    }
+
+    public String getTire() {
+        return tire;
+    }
+
+    public Car setTire(String tire) {
+        this.tire = tire;
+        return this;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,80 @@
+import java.security.Key;
+
 public class Main {
     public static void main(String[] args) {
+        //      String transmission;
+        //    final String bodyType = null;
+        //    final int numberOfSeats = 0;
+        //    String regNumber;
+        //    String tire;
 
-        Car лада = new Car(null, "", -0.5,
-                " ", -9, "Russia");
-        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "чёрный",
-                2020,"Германия");
-        Car bmw = new Car("BMW", "Z8", 3.0, "чёрный",
-                2021,"Германия");
-        Car kia = new Car("Kia", "Sportage 4", 2.4, "красный",
-                2018,"Южная Корея");
-        Car hunday = new Car("Hyundai", "Avante", 1.6, "оранжевый",
-                2016,"Южная Корея");
-        System.out.println(лада);
+        Car lada = new Car(
+                "Лада",
+                "",
+                -0.5,
+                " ",
+                -9,
+                "Russia",
+                " ",
+                "лифтбек",
+                8,
+                "A896EY076",
+                "зима",
+                new Car.Key("да", "нет"));
+        lada.setTire("лето"); // замена резины
+        Car audi = new Car(
+                "Audi",
+                "A8 50 L TDI quattro",
+                3.0,
+                "чёрный",
+                2020,
+                "Германия",
+                "АКПП",
+                "хэтчбек",
+                5,
+                "",
+                "лето",
+                new Car.Key("да", ""));
+        Car bmw = new Car(
+                "BMW",
+                "Z8",
+                3.0,
+                "чёрный",
+                2021,
+                "Германия",
+                "АКПП",
+                "хетчбек",
+                5,
+                "",
+                "",new Car.Key("да","да"));
+
+        Car kia = new Car(
+                "Kia",
+                "Sportage 4",
+                2.4,
+                "красный",
+                2018,
+                "Южная Корея",
+                "МКПП",
+                "хэтчбек",
+                5,
+                "",
+                "",
+                new Car.Key("да", "нет"));
+        Car hunday = new Car(
+                "Hyundai",
+                "Avante",
+                1.6,
+                "оранжевый",
+                2016,
+                "Южная Корея",
+                "АКПП",
+                "седан",
+                5,
+                "",
+                "",
+                new Car.Key("", ""));
+        System.out.println(lada);
         System.out.println(audi);
         System.out.println(bmw);
         System.out.println(kia);
